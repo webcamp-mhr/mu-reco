247762f697b686f2c628e83429123e1acec404e4
Yoshihiro-HosokawaX <hosokawa.yoshihiro.infratop@gmail.com>
2019-07-14 06:41:41 +0000

    テーブルのアソシエーション追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+
+  belongs_to :label
+  belongs_to :genre
+  belongs_to :artist
+  has_many :discs
+  has_many :carts
 end
--- app/models/purchase_history.rb
+++ app/models/purchase_history.rb
@@ -1,2 +1,5 @@
 class PurchaseHistory < ApplicationRecord
+
+  belongs_to :user
+  has_many :purchases_products
 end
