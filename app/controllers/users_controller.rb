b4fcd2235a1e908f93b2c949d9f3626e37c46d62
Kajitani <k.ryunosuke0430@gmail.com>
2019-07-20 21:47:30 +0900

    ユーザー一覧

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
 
 	def index
+    @users = User.all.page(params[:page]).reverse_order
 	end
 
 	def show
 		@user = User.find(params[:id])
+
 	end
 
 	def edit
@@ -18,7 +20,10 @@ class UsersController < ApplicationController
 
  	end
 
+  private
+
  	def user_params
  		params.require(:user).permit(:user_lastname, :user_firstname, :user_lastname_kana, :user_firstname_kana, :phone_number, :email)
  	end
+
 end
