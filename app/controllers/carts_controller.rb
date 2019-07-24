98fc752a39419bc65d2937ca7c8f9b51012e53e4
Kajitani <k.ryunosuke0430@gmail.com>
2019-07-24 21:00:20 +0900

    カートに追加

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,17 +1,34 @@
 class CartsController < ApplicationController
 
 	def destroy
+		cart = Cart.find(params[:id])
+		cart.destroy
+		redirect_to carts_path
 	end
 
 	def create
+		cart = Cart.new(cart_params)
+		cart.purchase_quantity = 1
+		if cart.save
+			redirect_to carts_path
+		end
+	end
+
+
+	def index
+		@carts = Cart.all
 	end
 
 	def update
+		cart = Cart.find(params[:id])
+		if cart.update(cart_params)
+			redirect_to carts_path
+		end
 	end
 
-	def index
+	private
+	def cart_params
+		params.require(:cart).permit(:product_id, :user_id, :purchase_quantity)
 	end
 
-	def edit
-	end
 end
